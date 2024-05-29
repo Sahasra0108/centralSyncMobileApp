64c827062eae33631b46eefadc5f1cd55a2ae398
Siluni <wijesinghe.anj@gmail.com>
2024-05-29 22:21:23 +0530

    create item list

---
+++ src/screens/Dashboard/AdminDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+const AdminDashboard=()=>{
+    return(
+        <FlatList>
+
+        </FlatList>
+    )
+}
+
+export default AdminDashboard;
